545ed00877c811a4ad29ad5b394baf30eabde244
Shaanshimpi <shaanshimpi@gmail.com>
2025-02-20 11:03:39 +0530

    ball annim removed

--- app/components/ScreenLoaderNew.tsx
+++ app/components/ScreenLoaderNew.tsx
@@ -3,34 +3,32 @@
 import { useEffect } from "react"
 import { disableScroll, enableScroll } from "@/lib/utils"
 import gsap from "gsap"
-import Typewriter from 'typewriter-effect';
-import './Loading.css'
+import Typewriter from "typewriter-effect"
+import "./Loading.css"
 
 export default function ScreenLoaderNew() {
-    
-    useEffect(() => {
+  useEffect(() => {
+    disableScroll()
 
-        disableScroll()
-    
-        gsap.to(".loader-screen", {
-          height: 0,
-          opacity:0,
-          duration: 1,
-          delay: 6,
-    
-          onComplete: () => {
-            enableScroll()
-            document.body.scrollTop = 0
-            gsap.set(".loader-screen", {
-              display: "none",
-            })
-          },
+    gsap.to(".loader-screen", {
+      height: 0,
+      opacity: 0,
+      duration: 1,
+      delay: 6,
+
+      onComplete: () => {
+        enableScroll()
+        document.body.scrollTop = 0
+        gsap.set(".loader-screen", {
+          display: "none",
         })
-      }, [])
-      
-    return (
-    <div className="loader-screen fixed z-50 flex h-screen w-screen items-center justify-center bg-black text-white text-[6vw]">
-        <div className="pendulum">
+      },
+    })
+  }, [])
+
+  return (
+    <div className="loader-screen fixed z-50 flex h-screen w-screen items-center justify-center bg-black text-[6vw] text-white">
+      {/* <div className="pendulum">
           <div className="pendulum_box">
             <div className="ball first"></div>
               <div className="ball"></div>
@@ -38,21 +36,22 @@ export default function ScreenLoaderNew() {
               <div className="ball"></div>
               <div className="ball last"></div>
           </div>
-        </div>
-        <br />
-        <Typewriter
-            onInit={(typewriter) => {
-                typewriter.changeDelay(20)
-                .changeDeleteSpeed(5)
-                .typeString('Compiling creativity...')
-                .pauseFor(500)
-                .deleteChars(25)
-                .pauseFor(50)
-                .typeString('Transforming ideas into code.')
-                .pauseFor(500)
-                .start();
-            }}
-        />
+        </div> */}
+      <br />
+      <Typewriter
+        onInit={(typewriter) => {
+          typewriter
+            .changeDelay(20)
+            .changeDeleteSpeed(5)
+            .typeString("Compiling creativity...")
+            .pauseFor(500)
+            .deleteChars(25)
+            .pauseFor(50)
+            .typeString("Transforming ideas into code.")
+            .pauseFor(500)
+            .start()
+        }}
+      />
     </div>
-    )
+  )
 }
