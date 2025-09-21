ad422661a62470159e931acdd932c69c5689a91c
Shaan <shaanshimpi@gmail.com>
2025-09-21 17:04:25 +0530

    working on vivy

--- app/Learn/Tutorials/[langSlug]/[tutSlug]/components/lessons/CodeRearrangeLesson.tsx
+++ app/Learn/Tutorials/[langSlug]/[tutSlug]/components/lessons/CodeRearrangeLesson.tsx
@@ -110,6 +110,7 @@ const CodeRearrangeLesson: React.FC<CodeRearrangeLessonProps> = ({
       const newIndex = direction === "up" ? currentIndex - 1 : currentIndex + 1
 
       if (newIndex >= 0 && newIndex < newOrder.length) {
+        //no semicolon here
         ;[newOrder[currentIndex], newOrder[newIndex]] = [
           newOrder[newIndex],
           newOrder[currentIndex],
